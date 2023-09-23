d196e9080da130eb51a63f62d5accd6c2e88440f
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-09-23 16:31:31 +0530

    Add factorial method

--- solver.rb
+++ solver.rb
@@ -1,2 +1,11 @@
 class Solver
+  def factorial(number)
+    if number.negative?
+      'write a number is greater than or equal to 0'
+    elsif [0, 1].include?(number)
+      1
+    else
+      number * factorial(number - 1)
+    end
+  end
 end
